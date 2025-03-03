13da986ddb1d493b02f3a0db957618ced675da00
Umendra A Bisen <umendrabisen2005@gmail.com>
2025-03-03 09:43:14 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,90 @@
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
+// import {
+//   AiFillGithub,
+//   AiOutlineTwitter,
+//   AiFillInstagram,
+// } from "react-icons/ai";
+// import { FaLinkedinIn } from "react-icons/fa";
+
+// function Footer() {
+//   let date = new Date();
+//   let year = date.getFullYear();
+//   return (
+//     <Container fluid className="footer">
+//       <Row>
+//         <Col md="4" className="footer-copywright">
+//           <h3>Designed and Developed by Umendra Bisen</h3>
+//         </Col>
+//         <Col md="4" className="footer-copywright">
+//           <h3> {year} UB</h3>
+//         </Col>
+//         <Col md="4" className="footer-body">
+//           <ul className="footer-icons">
+//             <li className="social-icons">
+//               <a
+//                 href="https://github.com/UmendraBisen2"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillGithub />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href=""
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiOutlineTwitter />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.linkedin.com/in/umendra-bisen-96bb56253"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <FaLinkedinIn />
+//               </a>
+//             </li>
+//             <li className="social-icons">
+//               <a
+//                 href="https://www.instagram.com/umendra1842/"
+//                 style={{ color: "white" }}
+//                 target="_blank" 
+//                 rel="noopener noreferrer"
+//               >
+//                 <AiFillInstagram />
+//               </a>
+//             </li>
+//           </ul>
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// }
+
+// export default Footer;
+
+
+
+
+
+
+
+
+
+
+
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
+  AiOutlineYouTube,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -17,7 +99,7 @@ function Footer() {
           <h3>Designed and Developed by Umendra Bisen</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3> {year} UB</h3>
+          <h3> {year} SB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
@@ -33,17 +115,17 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href=""
+                href="https://www.youtube.com/@apshorts6121"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                <AiOutlineTwitter />
+                <AiOutlineYouTube/>
               </a>
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/umendra-bisen-96bb56253"
+                href="https://www.linkedin.com/in/umendra-b-96bb56253"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -69,3 +151,4 @@ function Footer() {
 }
 
 export default Footer;
+
