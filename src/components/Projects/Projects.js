5ec8f3a85618f23cbe56c7b07eadb1434fc6228a
Umendra A Bisen <umendrabisen2005@gmail.com>
2025-03-12 16:49:28 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,45 +1,14 @@
+
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/";
-import emotion from "../../Assets/Projects/";
-import editor from "../../Assets/Projects/";
-import chatify from "../../Assets/Projects/";
-import suicide from "../../Assets/Projects/";
-import bitsOfCode from "../../Assets/Projects/";
-
-
-
-
-
-
-// import leaf from "../../Assets/Projects/leaf.png";
-// import emotion from "../../Assets/Projects/emotion.png";
-// import editor from "../../Assets/Projects/codeEditor.png";
-// import chatify from "../../Assets/Projects/chatify.png";
-// import suicide from "../../Assets/Projects/suicide.png";
-// import bitsOfCode from "../../Assets/Projects/blog.png";
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { Container, Row, Col } from "react-bootstrap";
-// import ProjectCard from "./ProjectCards";
-// import Particle from "../Particle";
-// import leaf from "../../Assets/Projects/Pizza.png";
-// import emotion from "../../Assets/Projects/Stack.png";
-// import editor from "../../Assets/Projects/tatto.png";
-// import chatify from "../../Assets/Projects/shoes.png";
-// import suicide from "../../Assets/Projects/quiz.png";
-// import bitsOfCode from "../../Assets/Projects/calcu.png";
+import leaf from "../../Assets/Projects/Pizza.png";
+import emotion from "../../Assets/Projects/Stack.png";
+import editor from "../../Assets/Projects/tatto.png";
+import chatify from "../../Assets/Projects/shoes.png";
+import suicide from "../../Assets/Projects/quiz.png";
+import bitsOfCode from "../../Assets/Projects/calcu.png";
 
 
 
